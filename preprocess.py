70adc3c61b85ccbea264bf9405305b74a3f022cf
yarikmenchaca <Yarik>
2021-07-14 22:19:08 -0500

    affective text

--- preprocess.py
+++ preprocess.py
@@ -6,6 +6,8 @@ from typing import Iterable
 from pathlib import Path
 from itertools import chain
 import pandas as pd
+import numpy as np
+import xml.etree.ElementTree as ET
 
 
 class EmotionStimulus:
@@ -81,3 +83,53 @@ class ISEAR:
     def get_sentence_tags(self) -> Iterable[tuple]:
         raw_data = pd.read_csv(self.csv_file)
         return list(raw_data[['content', 'sentiment']].to_records(index=False))
+
+
+class AffectiveText:
+    def __init__(self, dir_path: str):
+        self._train = Path(dir_path, 'AffectiveText.test')
+        self._test = Path(dir_path, 'AffectiveText.trial')
+        self.sentence_tags: tuple = self.get_sentence_tags()
+        self.sentences: list = [sentence for sentence, _ in self.sentence_tags]
+        self.tags: list = [tag for _, tag in self.sentence_tags]
+        self.emotions: set = set(self.tags)
+
+    def get_sentence_tags(self):
+        tree = ET.parse(self._train.joinpath('affectivetext_test.xml'))
+        root = tree.getroot()
+
+        with open(self._train.joinpath('affectivetext_test.emotions.gold'),
+                  'r') as file:
+            emotion_dict = {row[0]: row[1:] for row in
+                            map(str.split, map(str.strip, file.readlines()))}
+
+        train_data = self.generate_text_emotion_tuple(root, emotion_dict)
+
+        # Test Data
+
+        tree_test = ET.parse(self._test.joinpath('affectivetext_trial.xml'))
+        root_test = tree_test.getroot()
+        with open(self._test.joinpath('affectivetext_trial.emotions.gold'),
+                  'r') as file:
+            emotion_dict_test = {row[0]: row[1:] for row in map(str.split,
+                                                                map(str.strip,
+                                                                    file.readlines()))}
+
+        test_data = self.generate_text_emotion_tuple(root_test,
+                                                     emotion_dict_test)
+        return train_data + test_data
+
+    def generate_text_emotion_tuple(self, tree_root, emo_dict: dict) -> list:
+
+        emotions_order = ['anger', 'disgust', 'fear', 'joy', 'sadness',
+                          'surprise']
+        emotion_text_tuple = list()
+
+        for element in tree_root.findall('instance'):
+            t_id = element.attrib['id']
+            text = element.text
+            if max(list(map(int, emo_dict[t_id]))) != 0:
+                emotion = emotions_order[
+                    np.argmax(list(map(int, emo_dict[t_id])))]
+                emotion_text_tuple.append((text, emotion))
+        return emotion_text_tuple
